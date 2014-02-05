149bb45e408e41a1985da5cd580302511de6fafb
Ichigo Mayo <esetomo@gmail.com>
2014-02-05 23:13:23 +0900

    ファイル入出力

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -1,4 +1,5 @@
-﻿using MMF.CG;
+﻿using Microsoft.Win32;
+using MMF.CG;
 using MMF.CG.DeviceManager;
 using MMF.CG.Model.MMD;
 using MMF.CG.Motion;
@@ -17,12 +18,66 @@ namespace VMDToBVH.ViewModels
     {
         public MainViewModel()
         {
-            convertCommand = new DelegateCommand(ConvertCommandExecute);
+            openPmxCommand = new DelegateCommand(OpenPmxCommandExecute, OpenPmxCommandCanExecute);
+            openVmdCommand = new DelegateCommand(OpenVmdCommandExecute, OpenVmdCommandCanExecute);
+            convertCommand = new DelegateCommand(ConvertCommandExecute, ConvertCommandCanExecute);
+            saveBvhCommand = new DelegateCommand(SaveBvhCommandExecute, SaveBvhCommandCanExecute);
+        }
+
+        private bool OpenPmxCommandCanExecute(object arg)
+        {
+            return renderContext != null;
+        }
+
+        private void OpenPmxCommandExecute(object obj)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "PMXモデルファイル(*.pmx)|*.pmx";
+            if (ofd.ShowDialog() == true)
+            {
+                Model = MMDModel.OpenLoad(ofd.FileName, renderContext);
+            }
+        }
+
+        private bool OpenVmdCommandCanExecute(object arg)
+        {
+            return model != null;
+        }
+
+        private void OpenVmdCommandExecute(object obj)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "VMDモーションファイル(*.vmd)|*.vmd";
+            if (ofd.ShowDialog() == true)
+            {
+                Motion = model.MotionManager.AddMotionFromFile(ofd.FileName, true);
+            }
+        }
+
+        private bool ConvertCommandCanExecute(object arg)
+        {
+            return motion != null;
         }
 
         private void ConvertCommandExecute(object param)
         {
             BVH = new BVH(model, motion, (frame) => CurrentFrame = frame);
+        }
+
+        private bool SaveBvhCommandCanExecute(object arg)
+        {
+            return bvh != null;
+        }
+
+        private void SaveBvhCommandExecute(object obj)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "BVHファイル(*.bvh)|*.bvh";
+            if (sfd.ShowDialog() == true)
+            {
+                using (var stream = sfd.OpenFile())
+                    bvh.Save(stream);
+            }
         }
 
         private RenderContext renderContext;
@@ -35,7 +90,6 @@ namespace VMDToBVH.ViewModels
             set
             {
                 renderContext = value;
-                Model = MMDModel.OpenLoad(@"X:\mmd\models\a.pmx", renderContext);                
 
                 RaisePropertyChanged();
                 RaisePropertyChanged(() => Model);
@@ -52,7 +106,6 @@ namespace VMDToBVH.ViewModels
             set
             {
                 worldSpace = value;
-                worldSpace.AddResource(model);
                 RaisePropertyChanged();
             }
         }
@@ -83,7 +136,8 @@ namespace VMDToBVH.ViewModels
             set
             {
                 model = value;
-                Motion = model.MotionManager.AddMotionFromFile(@"X:\mmd\motions\test.vmd", true);
+                worldSpace.AddResource(model);
+                Motion = null;
 
                 RaisePropertyChanged();
             }
@@ -100,9 +154,12 @@ namespace VMDToBVH.ViewModels
             {
                 motion = value;
 
-                motion.FrameTicked += (_, e) => RaisePropertyChanged(() => CurrentFrame);
-                model.MotionManager.ApplyMotion(motion, 0);
-                motion.Stop();
+                if (motion != null)
+                {
+                    motion.FrameTicked += (_, e) => RaisePropertyChanged(() => CurrentFrame);
+                    model.MotionManager.ApplyMotion(motion, 0);
+                    motion.Stop();
+                }
 
                 RaisePropertyChanged();
                 RaisePropertyChanged(() => FinalFrame);
@@ -181,6 +238,33 @@ namespace VMDToBVH.ViewModels
             get
             {
                 return convertCommand;
+            }
+        }
+
+        private readonly ICommand openPmxCommand;
+        public ICommand OpenPmxCommand
+        {
+            get
+            {
+                return openPmxCommand;
+            }
+        }
+
+        private readonly ICommand openVmdCommand;
+        public ICommand OpenVmdCommand
+        {
+            get
+            {
+                return openVmdCommand;
+            }
+        }
+
+        private readonly ICommand saveBvhCommand;
+        public ICommand SaveBvhCommand
+        {
+            get
+            {
+                return saveBvhCommand;
             }
         }
     }
