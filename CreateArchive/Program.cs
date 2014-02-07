02c43044af25e2e2ba6af8c020e75bdfad4a103f
Ichigo Mayo <esetomo@gmail.com>
2014-02-08 04:35:48 +0900

    PowerShellはよーわからん

---
+++ CreateArchive/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Compression;
+using System.IO;
+
+namespace CreateArchive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dir = args[0];
+            var filename = args[1];
+
+            if (File.Exists(filename))
+                File.Delete(filename);
+
+            using (var archive = ZipFile.Open(filename, ZipArchiveMode.Create))
+            {
+                foreach (var file in Directory.GetFiles(dir, "*.*", SearchOption.AllDirectories))
+                {
+                    if (Path.GetExtension(file) != ".pdb")
+                    {
+                        System.Console.WriteLine(file.Replace(dir, ""));
+                        archive.CreateEntryFromFile(file, file.Replace(dir, ""));
+                    }
+                }
+            }
+        }
+    }
+}
